29f8875ea2a42d86f6fbbef283da233e16bf7835
LukeJSimmons <lucasjsimmons@yahoo.com>
2025-03-11 07:03:41 -0500

    Add createGrid function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello world")
+function createGrid(size=16) {
+    const container = document.querySelector(".container")
+
+    for (let i = 0; i < size*size; i++) {
+        const div = document.createElement("div")
+        div.classList.add("grid_item")
+        container.appendChild(div)
+    }
+}
+
+createGrid()
