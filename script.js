e240f86bd390d0270ec173fd39228c232c10694a
LukeJSimmons <lucasjsimmons@yahoo.com>
2025-03-11 07:23:43 -0500

    Add variable size and button
    The size of the canvas can now be changed freely and it will add as many grid items as needed.

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 function createGrid(size=16) {
+    // Ensures user can't make an empty canvas or overload the system with 1000 elements
+    if (!isValidSize(size)) return alert("Invalid size! Must be between 1-100")
+
     const container = document.querySelector(".container")
 
     for (let i = 0; i < size*size; i++) {
         const div = document.createElement("div")
         div.classList.add("grid_item")
+        div.style.cssText = `width: calc(100% / ${size});`
         container.appendChild(div)
     }
+
+    displaySize(size)
 }
 
-createGrid()
+function isValidSize(size) {
+    return size > 0 && size <= 100
+}
+
+function displaySize(size) {
+    const body = document.querySelector("body")
+
+    const sizeText = document.createElement("h2")
+    sizeText.innerText = `${size}x${size}`
+    body.appendChild(sizeText)
+}
+
+function linkGridButton() {
+    const button = document.querySelector("#grid_button")
+    button.addEventListener("click", pressGridButton)
+}
+
+function pressGridButton() {
+    console.log("press")
+}
+
+linkGridButton()
+
+createGrid(16)
